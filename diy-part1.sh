b3165c0b66e40fa4ce02fceb7c9e0a64a17b16f3
mxue <2578096874@qq.com>
2022-02-08 19:48:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+wget https://github.com/coolsnowwolf/lede/pull/8838.patch
+git apply 8838.patch
